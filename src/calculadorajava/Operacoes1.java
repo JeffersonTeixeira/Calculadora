820b3fc8c4a2d7631ed423cd78f53b97d18f3432
jessica <jessica@jessica-pc>
2017-06-10 16:51:27 -0300

    Primeiro2

---
+++ src/calculadorajava/Operacoes1.java
@@ -0,0 +1,15 @@
+package calculadorajava;
+public class Operacoes1 {
+   
+    
+    public int soma (int x, int y){
+        return x + y;
+    }
+    
+    public int subtracao (int a, int b){
+        return a - b;
+        
+        
+    }
+    
+}
