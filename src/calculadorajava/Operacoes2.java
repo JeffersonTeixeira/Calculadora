889fd1ba96162365cf0a97d0b9f0d358f4086ec0
jessica <jessica@jessica-pc>
2017-06-10 16:51:40 -0300

    Primeiro2

---
+++ src/calculadorajava/Operacoes2.java
@@ -0,0 +1,11 @@
+package calculadorajava;
+public class Operacoes2 {
+    
+    public int multiplicacao(int z, int w){
+    return z * w;
+}
+    public int divisao(int c, int d){
+        return c / d;
+    }
+
+}
