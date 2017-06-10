f2082e731b441b43b2521bdedc1db53851610f42
jessica <jessica@jessica-pc>
2017-06-10 16:50:56 -0300

    Primeiro2

--- src/calculadorajava/CalculadoraJava.java
+++ src/calculadorajava/CalculadoraJava.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculadorajava;
-
-/**
- *
- * @author Jefferson
- */
 public class CalculadoraJava {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Operacoes1 op1 = new  Operacoes1();
+        Operacoes2 op2 = new Operacoes2();
+        
+        System.out.println("O resultado da soma é: "+op1.soma(5, 4));
+        System.out.println("O resultado da subtração é: "+op1.subtracao(3, 2));
+        System.out.println("O resultado da multiplicação é: "+op2.multiplicacao(5, 5));
+        System.out.println("O resultado da divisão é: "+op2.divisao(10, 5));
     }
-    
+
+   
 }
